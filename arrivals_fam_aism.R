57c89bb104ae3655a39ac88a154aa191a898f5aa
rmatv <r_matv@protonmail.com>
2020-01-20 16:22:48 +0300

    remove loaded file

--- arrivals_fam_aism.R
+++ arrivals_fam_aism.R
@@ -24,6 +24,7 @@ setwd("~/aism/2019")
 # В случае необходимости обработать выгрузку из АИС "Отдых" скриптом
 # register_aiso.R (https://github.com/uasmgt/ais/blob/master/register_aiso.R).
 aiso.register <- get(load("~/data/aiso_vouchers_2019.rda"))
+rm(vouchers.aiso.2019)
 # Объединение серии и номера документа отдыхающего в одну переменную
 aiso.register$id <- paste(aiso.register$IDser_camper,
                           aiso.register$IDno_camper)
