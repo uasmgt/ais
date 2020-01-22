e7a8ddd62c0e83a17b2630184f061a44c30ef3ee
rmatv <10424604+rmatv@users.noreply.github.com>
2020-01-23 00:40:49 +0300

    minor changes, more to be done

--- ind2019.R
+++ ind2019.R
@@ -3,6 +3,9 @@ library(openxlsx)
 library(dplyr)
 library(tidyr)
 
+# Функции --------------------------------------------------------------
+source("~/git/ais/medical/medical_functions.R")
+
 # Дополнительные данные ------------------------------------------------
 # Данные из АИСО
 aiso.register <- get(load("~/data/aiso_vouchers_2019.rda"))
@@ -15,10 +18,10 @@ aiso.orphans <- aiso.register %>%
 aiso.needy <- aiso.register %>% 
   filter(benefit ==
            "Дети из малообеспеченных семей")
-
 # Реестр отказов 
 denials.data <- read.csv2("~/data/denials_ind2019.csv", 
 	encoding = "UTF-8")
-
 # Рабочая директория
 setwd("~/aism/2019/arrivals_ind")
+# Данные о лагерях
+camps <- read.csv2("~/data/camps.csv")
